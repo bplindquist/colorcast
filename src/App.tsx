f94ba3142d288d2b6d29a10637d596ba151f2f9c
Brian Lindquist <brian.p.lindquist@gmail.com>
2025-11-30 14:42:55 -0600

    add location search input

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
+import { Input } from "@heeler/ui";
 import "./App.css";
+import { useState, type ChangeEvent } from "react";
 
 export const App = () => {
+  const [locationSearch, setLocationSearch] = useState("");
+
+  const handleLocationChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setLocationSearch(e.target.value);
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleSearch();
+      setLocationSearch("");
+    }
+  };
+
+  const handleSearch = () => {
+    console.log("Searching for:", locationSearch);
+  };
+
   return (
     <>
-      <h1>Colorcast Weather</h1>
+      <header className="flex justify-end px-6 py-4 border-b border-gray-200">
+        <div className="flex gap-2 items-center">
+          <Input
+            color="blue"
+            placeholder="Location"
+            value={locationSearch}
+            onChange={handleLocationChange}
+            onKeyDown={handleKeyDown}
+          />
+        </div>
+      </header>
     </>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import { App } from "./App.tsx";
+import "@heeler/ui/styles.css";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
