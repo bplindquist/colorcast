89f944063600e4d8c28aa1207a271c0f197a4cc3
Brian Lindquist <brian.p.lindquist@gmail.com>
2025-11-30 14:53:18 -0600

    Add location search input (#1)
    * add location search input
    
    * update styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,44 @@
+import { useState, type ChangeEvent } from "react";
+import { Input } from "@heeler/ui";
 import "./App.css";
 
 export const App = () => {
+  const [locationSearch, setLocationSearch] = useState("");
+
+  const handleLocationChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setLocationSearch(e.target.value);
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleSearch();
+      setLocationSearch("");
+    }
+  };
+
+  const handleSearch = () => {
+    console.log("Searching for:", locationSearch);
+  };
+
   return (
     <>
-      <h1>Colorcast Weather</h1>
+      <header className="flex justify-between items-center px-6 py-4">
+        <div>
+          <h2 className="text-2xl font-bold text-blue-700">Colorcast</h2>
+          <h3 className="text-lg text-gray-600 text-orange-600">
+            Weather at a colorful glance
+          </h3>
+        </div>
+        <div className="flex gap-2 items-center">
+          <Input
+            color="blue"
+            placeholder="City or zip code"
+            value={locationSearch}
+            onChange={handleLocationChange}
+            onKeyDown={handleKeyDown}
+          />
+        </div>
+      </header>
     </>
   );
 };
